2832c7dc9f61ccea22fa21df85aeac241016d5ab
Bitterulf <s_schlender@hotmail.com>
2014-10-26 12:17:01 +0100

    Controllig video.js.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var $ = require('jquery');
+var $ = require('jQuery');
 
 var template = require('./templates/hackathon.jade');
 
@@ -8,11 +8,18 @@ $( document ).ready(function() {
     .append('<script src="http://code.jquery.com/jquery-2.1.1.min.js" />')
     .append('<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" />')
     .append('<script src="/js/phaser.min.js" />')
-    .append('<link rel="stylesheet" href="//example.com/path/to/video-js.css" type="text/css" />')
-    .append('<script src="//example.com/path/to/video.js" />')
+    .append('<link rel="stylesheet" href="//vjs.zencdn.net/4.9/video-js.css" type="text/css" />')
+    .append('<script src="//vjs.zencdn.net/4.9/video.js" />')
     .append('<script src="/js/game.js" />')
     .append('<link rel="stylesheet" href="css/main.css" type="text/css" />');
 
   $( 'title' ).html('hackathon');
   $( 'body' ).html(template());
+  setTimeout(function(){
+    videojs("example_video_1").ready(function(){
+      var myPlayer = this;
+      myPlayer.currentTime(30);
+      myPlayer.play();
+    });
+  }, 3000);
 });
