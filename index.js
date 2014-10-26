1fca25b35119456d113e0bfb5e2ebf0540f96720
Bitterulf <s_schlender@hotmail.com>
2014-10-26 10:08:11 +0100

    Add bootstrap.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 $ = require('jquery');
 
 $( document ).ready(function() {
-  $('head').append('<link rel="stylesheet" href="css/main.css" type="text/css" />');
+  $('head').append('<link rel="stylesheet" href="css/main.css" type="text/css" />')
+    .append('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" type="text/css" />')
+    .append('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css" type="text/css" />')
+    .append('<script src="http://code.jquery.com/jquery-2.1.1.min.js" />')
+    .append('<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js" />');
+
   $( 'title' ).html('hackathon');
+  $( 'body' ).html('<div class="container"><div class="col-md-12">hackathon</div></div>');
 });
