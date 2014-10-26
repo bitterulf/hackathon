aeb44c55797db2e33b217533fc7ac3a7cea141d2
Bitterulf <s_schlender@hotmail.com>
2014-10-26 09:55:21 +0100

    Add jquery.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('fooo');
+$ = require('jquery');
+
+$( document ).ready(function() {
+  $('head').append('<link rel="stylesheet" href="css/main.css" type="text/css" />');
+  $( 'title' ).html('hackathon');
+});
